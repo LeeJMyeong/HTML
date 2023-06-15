8b0475e87751485bc5b30bf0833a798d0819c1ee
leejmyeong <zdlwoaud@naver.com>
2023-06-15 12:05:16 +0900

    user post success

--- src/main/java/com/palydata/todos/config/JdbcConnection.java
+++ src/main/java/com/palydata/todos/config/JdbcConnection.java
@@ -3,9 +3,8 @@ package com.palydata.todos.config;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 public class JdbcConnection {
-        private static final String URL = "jdbc:mysql://localhost:3306/horse_racing_db"
+        private static final String URL = "jdbc:mysql://localhost:3306/todos"
                 + "?serverTimezone=Asia/Seoul&characterEncoding=UTF-8";
         private static final String USER = "root";
         private static final String PASSWORD = "12345678";
@@ -18,5 +17,17 @@ public class JdbcConnection {
             }
             return conn;
         }
+
+    public Connection getJdbc() {
+        Connection conn;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        }catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+//System.out.println("연동 성공");
+        return conn;
     }
+}
 
